7f7c8f89511b1332fd4527346c951ecd96353e42
Neel <stepheng@teezlelabs.com>
2020-12-25 12:50:04 +0530

    side nav chnaged and add a header.js

---
+++ webapps/js/header.js
@@ -0,0 +1,11 @@
+$('#myNav .overlay-content .nav-link').on('click', function(){
+      document.getElementById("myNav").style.width = "0%";
+    });
+  function openNav() {
+    document.getElementById("myNav").style.width = "15%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+  
