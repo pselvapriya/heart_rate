66a5f3f2e293ddaac6f960ef5e66f1bee53dd81d
Booma4827 <mypersonal4827@gmail.com>
2020-12-26 13:02:56 +0530

    update datepicker

--- webapps/js/patients-profile.js
+++ webapps/js/patients-profile.js
@@ -6,6 +6,21 @@ $(function() {
     function cb(start, end) {
         $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
     }
+    $('#reportrange').daterangepicker({
+        startDate: start,
+        endDate: end,
+        ranges: {
+            'Today': [moment(), moment()],
+            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+            'This Month': [moment().startOf('month'), moment().endOf('month')],
+            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        }
+    }, cb);
+
+    cb(start, end);
+
 });
 
 function myFunction() {
@@ -48,4 +63,14 @@ $(document).ready(function() {
 
         ]
     });
+});
+$(".js-range-slider").ionRangeSlider({
+    skin: "flat",
+    grid: true,
+    min: 0,
+    max: 100,
+    from: 21,
+    max_postfix: "+",
+    prefix: "Age: ",
+    postfix: " years"
 });
