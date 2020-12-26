655b1d586ba931714aaccc5f639cc7b11a29aebd
pselvapriya <pselvapriya1998.com>
2020-12-25 23:45:52 -0800

    proper code

---
+++ webapps/js/login.js
@@ -0,0 +1,23 @@
+function gotoHome(){
+  window.location.href='http://localhost:7201/hrmonitor/main';
+}
+// var attempt = 3;
+// function gotoHome(){
+// var eid = document.getElementById("").value;
+// var password = document.getElementById("password").value;
+// if ( eid == "vilpower@boodskap.io" && password == "welcome123"){
+// alert ("Login successfully");
+// window.location.href='http://localhost:7201/hrmonitor/main';
+// return false;
+// }
+// else{
+// attempt
+// alert("You have left "+attempt+" attempt;");
+// if( attempt == 0){
+// document.getElementById("eid").disabled = true;
+// document.getElementById("password").disabled = true;
+// document.getElementById("sign me").disabled = true;
+// return false;
+// }
+// }
+// }
