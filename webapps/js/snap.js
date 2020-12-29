23e363f418fe490bc6e0368cf7b3090e287e30db
chermaraja <chermaraja>
2020-12-29 10:26:10 +0530

    chage file

--- webapps/js/snap.js
+++ webapps/js/snap.js
@@ -44,3 +44,16 @@ $(function() {
     cb(start, end);
 
 });
+
+var elem = document.getElementById("expand");
+var elem = document.documentElement;
+
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
