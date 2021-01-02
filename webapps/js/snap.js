2607bfd67e0f5fb6e27674d3e86c7f877c00e896
chermaraja <chermaraja>
2021-01-02 11:20:17 +0530

    change style

--- webapps/js/snap.js
+++ webapps/js/snap.js
@@ -1,48 +1,20 @@
-$(function() {
-
-    var start = moment().subtract(29, 'days');
-    var end = moment();
-
-    function cb(start, end) {
-        $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
-    }
-    $('#reportrange').daterangepicker({
-        startDate: start,
-        endDate: end,
-        ranges: {
-            'Today': [moment(), moment()],
-            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-            'This Month': [moment().startOf('month'), moment().endOf('month')],
-            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-        }
-    }, cb);
-
-    cb(start, end);
-
-});
-
-// chart
-$(()=>{
-var myChart = echarts.init(document.querySelector('#chart'));
-var echartdata = [["2000-06-05",116],["2000-06-06",129],["2000-06-07",135],["2000-06-08",86],["2000-06-09",73],["2000-06-10",85],["2000-06-11",73],["2000-06-12",68],["2000-06-13",92],["2000-06-14",130],["2000-06-15",245],["2000-06-16",139],["2000-06-17",115],["2000-06-18",111],["2000-06-19",309],["2000-06-20",206],["2000-06-21",137],["2000-06-22",128],["2000-06-23",85],["2000-06-24",94],["2000-06-25",71],["2000-06-26",106],["2000-06-27",84],["2000-06-28",93],["2000-06-29",85],["2000-06-30",73],["2000-07-01",83],["2000-07-02",125],["2000-07-03",107],["2000-07-04",82],["2000-07-05",44],["2000-07-06",72],["2000-07-07",106],["2000-07-08",107],["2000-07-09",66],["2000-07-10",91],["2000-07-11",92],["2000-07-12",113],["2000-07-13",107],["2000-07-14",131],["2000-07-15",111],["2000-07-16",64],["2000-07-17",69],["2000-07-18",88],["2000-07-19",77],["2000-07-20",83],["2000-07-21",111],["2000-07-22",57],["2000-07-23",55],["2000-07-24",60],["2000-07-25",44],["2000-07-26",127],["2000-07-27",114],["2000-07-28",86],["2000-07-29",73],["2000-07-30",52],["2000-07-31",69],["2000-08-01",86],["2000-08-02",118],["2000-08-03",56],["2000-08-04",91],["2000-08-05",121],["2000-08-06",127],["2000-08-07",78],["2000-08-08",79],["2000-08-09",46],["2000-08-10",108],["2000-08-11",80],["2000-08-12",79],["2000-08-13",69],["2000-08-14",80],["2000-08-15",105],["2000-08-16",119],["2000-08-17",105],["2000-08-18",55],["2000-08-19",74],["2000-08-20",41],["2000-08-21",62],["2000-08-22",104],["2000-08-23",118],["2000-08-24",121],["2000-08-25",126],["2000-08-26",99],["2000-08-27",92],["2000-08-28",75],["2000-08-29",91],["2000-08-30",94],["2000-08-31",69],["2000-09-01",93],["2004-07-25",78],["2004-07-26",78],["2004-07-27",59],["2004-07-28",111],["2004-07-29",78],["2004-07-30",30],["2004-07-31",78],["2004-08-01",91],["2004-08-02",119],["2004-08-03",95],["2004-08-04",73],["2004-08-05",76],["2004-08-06",89],["2004-08-07",117],["2004-08-08",145],["2004-08-09",143],["2004-08-10",84],["2004-08-11",84],["2004-08-12",51],["2004-08-13",31],["2007-03-13",133]];
+var myChart = echarts.init(document.getElementById('chart'));
+var echartdata = [["2001-05-21",158],["2001-05-22",116],["2001-05-23",132],["2001-05-24",110],["2001-05-25",82],["2001-05-26",56],["2001-05-27",54],["2001-05-28",71],["2001-05-29",101],["2001-05-30",57],["2001-05-31",88],["2001-06-01",99],["2001-06-02",84],["2001-06-03",139],["2001-06-04",132],["2001-06-05",141],["2001-06-07",159],["2001-06-08",131],["2001-06-09",180],["2001-06-10",164],["2001-06-11",134],["2001-06-12",163],["2001-06-13",105],["2001-06-14",74],["2001-06-15",50],["2001-06-16",60],["2001-06-17",82],["2001-06-18",111],["2001-06-19",89],["2001-06-20",81],["2001-06-21",76],["2001-06-22",70],["2001-06-23",74],["2001-06-24",99],["2001-06-25",91],["2001-06-26",113],["2001-06-27",93],["2001-06-28",69],["2001-06-29",74],["2001-06-30",75],["2001-07-01",108],["2001-07-02",115],["2001-07-03",86],["2001-07-04",67],["2001-07-05",68],["2001-07-06",74],["2001-07-07",69],["2001-07-08",95],["2001-07-09",99],["2001-07-10",92],["2001-07-11",84],["2004-05-30",66],["2004-05-31",56],["2004-06-01",100],["2004-06-02",109],["2004-06-03",118],["2004-06-04",107],["2004-06-05",74],["2004-06-06",58],["2004-06-07",88],["2004-06-08",100],["2007-03-13",13]];
 
 myChart.setOption({
-title: {
-    text: 'Heartrate Analysis'
-    
-},
+
 tooltip: {
     trigger: 'axis'
 },
 xAxis: {
-    data: echartdata.map(function (item) {
+    data: echartdata.map(function(item) {
         return item[0];
     })
 },
 yAxis: {
+   
     splitLine: {
+        color: ["#3398DB"],
         show: false
     }
 },
@@ -66,34 +38,33 @@ visualMap: {
     right: 10,
     pieces: [{
         gt: 0,
-        lte: 50,
-        color: '#096'
+        lte: 60,
+        color: 'orange'
     }, {
-        gt: 50,
+        gt: 60,
         lte: 100,
-        color: '#ffde33'
+        color: 'yellow'
     }, {
-        gt: 100,
-        lte: 150,
-        color: '#ff9933'
-    }, {
-        gt: 150,
-        lte: 200,
-        color: '#cc0033'
-    }, {
-        gt: 200,
-        color: '#660099'
-    
-    }],
+        gt: 110,
+        
+        color: 'red'
+    },],
     outOfRange: {
         color: '#999'
     }
 },
 series: {
-    name: 'Heartrate Analysis',
+    markPoint: {
+        data: [
+            {type: 'max', name: 'booma'},
+            {type: 'min', name: 'booma'}
+        ]
+    },
+    name: 'heart rate',
     type: 'line',
-    data: echartdata.map(function (item) {
+    data: echartdata.map(function(item) {
         return item[1];
+       
     }),
     markLine: {
         silent: true,
@@ -105,210 +76,28 @@ series: {
             yAxis: 150
         }, {
             yAxis: 200
-        
+        }, {
+            yAxis: 300
         }]
     }
 }
 });
 
-var patientTable = null;
-var patient_list = [];
-// var startDate = moment().subtract(6, 'days').startOf('day');
-// var endDate = moment().endOf('day');
-
-$(document).ready(function(){
-    loadPatientList();
-})
 
 
+var colorThreshold = 10,
+spanText = document.querySelector('heartimg');
 
-function loadPatientList() {
+function changeColor(val) {
+var color = "green";
 
-    if (patientTable) {
-      patientTable.destroy();
-        $("#snap").html("");
-    }
-
-    var fields = [
-        {
-            mData: 'heart_rate',
-            sTitle: 'Heart Rate',
-            sWidth: '20%',
-            orderable: false,
-            mRender: function (data, type, row) {
-                return data;
-            }
-        },
-        {
-            mData: 'activity',
-            sTitle: 'Activity',
-            sWidth: '20%',
-            orderable: false,
-            mRender: function (data, type, row) {
-                return data;
-            }
-        },
-
-        {
-            mData: 'did',
-            sWidth: '20%',
-            sTitle: 'skin patch ID',
-            orderable: false,
-            mRender: function (data, type, row) {
-                return data;
-            }
-        },
-       
-        {
-            mData: 'reported_ts',
-            sTitle: 'Reported Time',
-            "className": 'sortingtable',
-            orderable: false,
-            mRender: function (data, type, row) {
-                return moment(data).format(DATE_TIME_FORMAT);
-            }
-        },
-        // {
-        //     sTitle: 'Actions',
-        //     orderable: false,
-        //     mRender: function (data, type, row) {
-        //         var actionsHtml = '<button class="btn btn-default" onclick="deleteDevice(\'' + row.id + '\')"><i class="fa fa-trash"></i></button>'+" "+'<button class="btn btn-default" onclick="editDevice(\'' + row._id + '\')"><i class="fa fa-edit"></i></button>';
-        //         return actionsHtml;
-        //     }
-        // }
-       
-    ];
-    
-
-    var queryParams = {
-        query: {
-            "bool": {
-                "must": []
-            }
-        },
-        sort: [{ "created_ts": { "order": "asc" } }]
-    };
-
-    patient_list = [];
-
-    var tableOption = {
-        fixedHeader: false,
-        responsive: false,
-        paging: true,
-        searching: true,
-        aaSorting: [[3, 'desc']],
-        "ordering": true,
-        iDisplayLength: 10,
-        lengthMenu: [[10, 50, 100], [10, 50, 100]],
-        aoColumns: fields,
-        "bProcessing": true,
-        "language": {
-            "emptyTable": "No data found!",
-            "processing": '<i class="fa fa-spinner fa-spin" style="color:#333"></i> Processing'
-
-        },
-        "bServerSide": true,
-        "sAjaxSource": BASE_PATH+'/patienthistory/list',
-        "fnServerData": function (sSource, aoData, fnCallback, oSettings) {
-
-
-            queryParams.query['bool']['must'] = [];
-            queryParams.query['bool']['should'] = [];
-            delete queryParams.query['bool']["minimum_should_match"];
-
-            var keyName = fields[oSettings.aaSorting[0][0]]
-
-            var sortingJson = {};
-            sortingJson[keyName['mData']] = { "order": oSettings.aaSorting[0][1] };
-            queryParams.sort = [sortingJson];
-
-            queryParams['size'] = oSettings._iDisplayLength;
-            queryParams['from'] = oSettings._iDisplayStart;
-
-            // queryParams.query['bool']['must'].push({ "match": { "acc_id":SESSION_OBJ.orgs[0]  } });
-
-            var searchText = oSettings.oPreviousSearch.sSearch.trim();
-
-            if (searchText) {
-                queryParams.query['bool']['should'].push({ "wildcard": { "activity": "*" + searchText + "*" } });
-                queryParams.query['bool']['should'].push({ "wildcard": { "activity": "*" + searchText.toLowerCase() + "*" } });
-                queryParams.query['bool']['should'].push({ "wildcard": { "activity": "*" + searchText.toUpperCase() + "*" } });
-                queryParams.query['bool']['should'].push({ "wildcard": { "activity": "*" + capitalizeFLetter(searchText) + "*" } })
-                queryParams.query['bool']["minimum_should_match"] = 1;
-                queryParams.query['bool']['should'].push({
-                    "match_phrase": {
-                        "activity.keyword": "*" + searchText + "*"
-                    }
-                })
-                queryParams.query['bool']['should'].push({
-                    "match_phrase_prefix": {
-                        "activity.keyword": {
-                            "query": "*" + searchText + "*"
-                        }
-                    }
-                });
-            }
-
-            oSettings.jqXHR = $.ajax({
-                "dataType": 'json',
-                "contentType": 'application/json',
-                "type": "post",
-                "url": sSource,
-                "data": JSON.stringify({"query":queryParams}),
-                success: function (data) {
-
-                    console.log(data);
-
-                    var resultData = data.result.data;
-
-                    device_list = resultData.data;
-
-                    $(".totalCount").html(data.result.total)
-
-                    resultData['draw'] = oSettings.iDraw;
-                    fnCallback(resultData);
-                }
-            });
-        },
-      
-    };
-
-    DeviceTable = $("#snap").DataTable(tableOption);
+if (val > 30 && val < 60) {
+    color = "yellow";
+} else if (val >= 60) {
+    color = "red";
 }
-$(function() {
 
-    var start = moment().subtract(29, 'days');
-    var end = moment();
-
-    function cb(start, end) {
-        $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
-    }
-    $('#reportrange').daterangepicker({
-        startDate: start,
-        endDate: end,
-        ranges: {
-            'Today': [moment(), moment()],
-            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-            'This Month': [moment().startOf('month'), moment().endOf('month')],
-            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-        }
-    }, cb);
-
-    cb(start, end);
-
-});
-
-var elem = document.getElementById("expand");
-var elem = document.documentElement;
-
-function openFullscreen() {
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.webkitRequestFullscreen) { /* Safari */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE11 */
-    elem.msRequestFullscreen();
-  }
+spanText.style.color = color;
 }
+
+changeColor(colorThreshold);
