feffa432d1e234deaeb05b2685fa68e6db2e80ee
Booma4827 <mypersonal4827@gmail.com>
2021-01-04 10:53:41 +0530

    add history file

--- webapps/js/patientstatus.js
+++ webapps/js/patientstatus.js
@@ -19,7 +19,7 @@ function loadPatientstatusList() {
             sWidth: '100px',
             orderable: false,
             mRender: function(data, type, row) {
-                return '<div class="row">' + '<img src="/images/Capture.PNG"style="height:30px;"width:30px">' + '&nbsp;' + '&nbsp;' + row.patient_name + '&nbsp;' + '<a href="#/snapshot">' + '&nbsp;' + '<i class="fa fa-eye eye-icon" aria-hidden="true"onclick="snapShot(\'' + row._id + '\' )"></i>' + '</a>' + '&nbsp;' + '<h6>' + '<b>' + '&nbsp;' + '&nbsp;' + row.age + '&nbsp;' + 'years old' + '</b>' + '</h6>' + '</div>';
+                return '<div class="row">' + '<img src="/images/Capture.PNG"style="height:30px;"width:30px">' + '&nbsp;' + '&nbsp;' + row.patient_name + '&nbsp;' + '&nbsp;' +'<a onclick="loadMainPage(\'/snapshot\')"href="#/snapshot">'+ '<i class="fa fa-eye eye-icon" aria-hidden="true"></i>' + '</a>'+'&nbsp;' + '<h6>' + '<b>' + '&nbsp;' + '&nbsp;' + row.age + '&nbsp;' + 'years old' + '</b>' + '</h6>' + '</div>';
             }
         },
         // {
@@ -261,3 +261,184 @@ function loadPatientstatusList() {
 
     PatientstatusTable = $("#patients_profile").DataTable(tableOption);
 }
+
+
+$(function() {
+
+    var start = moment().subtract(29, 'days');
+    var end = moment();
+
+    function cb(start, end) {
+        $('#reportrange span').html(start.format('MMM D, YYYY') + ' - ' + end.format('MMM D, YYYY'));
+    }
+    $('#reportrange').daterangepicker({
+        startDate: start,
+        endDate: end,
+        ranges: {
+            'Today': [moment(), moment()],
+            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+            'This Month': [moment().startOf('month'), moment().endOf('month')],
+            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        }
+    }, cb);
+
+    cb(start, end);
+
+});
+
+function myFunction() {
+    var x = document.getElementById("contt");
+    if (x.style.display === "block") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function hideFunction() {
+    var x = document.getElementById("contt");
+    if (x.style.display === "none") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+/* Get the documentElement (<html>) to display the page in fullscreen */
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen() {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+        document.msExitFullscreen();
+    }
+}
+
+
+
+
+
+$(document).ready(function() {
+    $('#patients_profile').DataTable({
+        data: dataSet,
+        searching: true,
+        columns: [{
+                title: 'Patient_Name',
+                sTitle: 'Patient Name',
+                orderable: false,
+                mRender: function(data, type, row) {
+                    return '<img src="/images/Capture.PNG"style="height:30px;"width:30px">' + row.patient_name + '&nbsp;' + '<a href="/hrmonitor/main#/snapshot">' + '<i class="fa fa-eye eye-icon" aria-hidden="true"></i>' + '</a>' + '&nbsp;' + '<h6>' + row.dob + 'years old</h6>';
+
+                }
+            },
+            // {
+            //     title: 'DOB',
+            //     sTitle: 'DOB',
+            //     orderable: false,
+            //     mRender: function(data, type, row) {
+            //         return data;
+            //     }
+            // },
+            // {
+            //     title: 'Age',
+            //     sTitle: 'Age',
+            //     orderable: false,
+            //     mRender: function(data, type, row) {
+            //         return data;
+            //     }
+            // },
+            // {
+            //     title: 'Gender',
+            //     sTitle: 'Gender',
+            //     orderable: false,
+            //     mRender: function(data, type, row) {
+            //         return data;
+            //     }
+            // },
+            {
+                title: 'Address',
+                sTitle: 'Address',
+                orderable: false,
+                mRender: function(data, type, row) {
+                    return data;
+                }
+            },
+            {
+                title: 'Heart Rate',
+                sTitle: 'Heart Rate',
+                orderable: false,
+                mRender: function(data, type, row) {
+                    return data + '&nbsp;' + '&nbsp;' + '&nbsp;' + '<i class="fa fa-icon fa-heart-rate" aria-hidden="true"></i>';
+                }
+            },
+            {
+                title: 'Status',
+                sTitle: 'Status',
+                orderable: false,
+                mRender: function(data, type, row) {
+                    return '<label class="status">' + data + '</label>';
+                }
+            },
+            {
+                title: 'Activity',
+                sTitle: 'Activity',
+                orderable: false,
+                mRender: function(data, type, row) {
+                    return data;
+                }
+            },
+            // {
+            //     mData: 'country',
+            //     sTitle: 'Country',
+            //     orderable: false,
+            //     mRender: function(data, type, row) {
+            //         return data;
+            //     }
+            // },
+            {
+                title: 'did',
+                sTitle: 'Skin Patch Id',
+                orderable: false,
+                mRender: function(data, type, row) {
+                    return data;
+                }
+            },
+            {
+                title: 'updated_ts',
+                sTitle: 'Last Reported Time',
+                orderable: false,
+                mRender: function(data, type, row) {
+                    return data;
+                }
+            },
+
+        ]
+
+    });
+});
+const reloadtButton = document.querySelector("#patients_profile");
+// Reload everything:
+function reload() {
+    reload = location.reload();
+}
+// Event listeners for reload
+reloadButton.addEventListener("click", reload, false);
+
+//slider js
